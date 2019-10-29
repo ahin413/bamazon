f38bd942bc16ab46cdb0261e4edfe348ea5b588d
Annemarie Hin <ame5033@gmail.com>
2019-10-29 19:18:01 -0400

    updated js file. total of purchase now working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,25 +1,68 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+    host: "localhost",
+    port: 3306,
 
- 
-  user: "root",
-  password: "Oliver413!",
-  database: "bamazon"
+
+    user: "root",
+    password: "Oliver413!",
+    database: "bamazon"
 });
 
-connection.connect(function(error){
-    if (error) throw error
-    console.log("connected as id " + connection.threadId);
-
-    //showProducts()
-});
-connection.query("SELECT * FROM products", function(err, res) {
+connection.connect(function(err) {
     if (err) throw err;
-    //console.log(res);
-    for (var i = 0; i < res.length; i++){
-        console.log (res[i].id + " |  Product:" + res[i].product_name + " | Deptartment: " + res[1].department_name + " | Price:" + res[i].price + " | Quantity:" + res[1].stock_quantity)};
+    //console.log("connected as id " + connection.threadId);
+
+    showProducts();
+});
+function showProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        //console.log(res);
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID:" + res[i].id + " |  Product:" + res[i].product_name + " | Deptartment: " + res[i].department_name + " | Price:" + res[i].price + " | Quantity:" + res[i].stock_quantity)
+        };
+
+        buyProduct()
+    })
+};
+
+var buyProduct = function () {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "productChoice",
+            message: "Please enter the item ID of the product you would like"
+        },
+        {
+            type: "input",
+            name: "quantityChoice",
+            message: "Please enter how many you would like to buy"
+        }
+    ]).then(function (purchase) {
+        connection.query("SELECT price, product_name, stock_quantity FROM products WHERE id = ?", [purchase.productChoice], function(err, res) {
+            var amountSold = purchase.quantityChoice;
+            var total = res[0].price * amountSold;
+            var itemsRemaining = (res[0].quantityChoice - amountSold);
+            if (err) {
+                return console.log("err");
+
+            }
+
+            if (res[0].quantityChoice < purchase.quantityChoice) {
+                return console.log("Sorry we do not have enough of that item");
+            }
+connection.query ("UPDATE products SET quantity = ? WHERE id = ?",[itemsRemaining, purchase.productChoice]);
+console.log ("Your order has been placed");
+console.log ("Your total is $" + total);
+
+connection.end();
+
+        });
+
     });
+
+}
 
--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE products (
     id INTEGER NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(50)NOT NULL,
     department_name VARCHAR(50) NOT NULL,
-    price DECIMAL(10,2) NULL,
+    price DECIMAL(10,2) NOT NULL,
     stock_quantity INTEGER(10) NOT NULL,
     PRIMARY KEY (id)
 );
